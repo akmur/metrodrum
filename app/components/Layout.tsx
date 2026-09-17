831fe8641f353a1499521b1b344742ff7cf55002
Alessandro Muraro <akmuraro@gmail.com>
2026-09-17 15:24:05 +0200

    add sticky footer with vibecode credit

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -3,11 +3,21 @@ import Navbar from "@/components/Navbar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
+    <div className="flex min-h-screen flex-col bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
       <Navbar />
-      <main className="container mx-auto px-4">
+      <main className="container mx-auto flex-1 px-4">
         <Outlet />
       </main>
+      <footer className="py-4 text-center text-sm" style={{ color: "rgb(64 78 96)" }}>
+        Vibecoded by{" "}
+        <a
+          href="https://ale.muraro.xyz"
+          className="underline"
+          style={{ color: "rgb(64 78 96)" }}
+        >
+          Alessandro Muraro
+        </a>
+      </footer>
     </div>
   );
 }
